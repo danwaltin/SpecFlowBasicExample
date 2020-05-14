7b3a55e908cd806820f99bf63a10b8c1d6be6300
Dan Waltin <dan.waltin@agero.se>
2020-05-14 08:23:07 +0200

    Adding empty bindings

---
+++ Requirements/Bindings/CustomerBindings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Requirements.Bindings
+{
+	[Binding]
+	public class CustomerBindings
+	{
+		[Given(@"en kund med följande uppgifter")]
+		public void GivetEnKundMedFoljandeUppgifter(Table table)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"kunden anonymiseras")]
+		public void NarKundenAnonymiseras()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"ska kundens uppgifter vara")]
+		public void SaSkaKundensUppgifterVara(Table table)
+		{
+			ScenarioContext.Current.Pending();
+		}
+	}
+}
